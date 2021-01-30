72a024209d87f092cb1cfe54d6bc78f4eb435bb2
Jrxnl3 <benhoffe1398@gmail.com>
2021-01-30 22:58:20 +0100

    Testing startswith

--- GPSTest.lua
+++ GPSTest.lua
@@ -11,18 +11,25 @@ while msg <= 1 do
     
     if message:startswith("MyX") then
         toX = message
+        print("Received " + toX)
     end
 
     if message:startswith("MyY") then
         toY = message
+        print("Received " + toY)
+
     end
 
     if message:startswith("MyZ") then
         toZ = message
+        print("Received " + toZ)
+
     end
 
     if message == "start" then
         msg = 1
+        print("Received " + msg)
+
     end
 
 end
